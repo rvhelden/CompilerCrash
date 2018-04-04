70bbb981359ead7fd084bc29d0ea2f095347074e
Ronald van Helden <R.v.Helden@HIS.Local>
2018-04-04 13:50:42 +0200

    Code

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CompilerCrash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public async Task OnCrash()
+        {
+            var switchObject = new object();
+            switch (switchObject)
+            {
+                case InvalidCastException _:
+                    switch (switchObject)
+                    {
+                        case NullReferenceException exception:
+
+                            await Task.CompletedTask;
+
+                            var myexception = exception;
+                            break;
+                    }
+
+                    break;
+                case InvalidOperationException _:
+                    switch (switchObject)
+                    {
+                        case NullReferenceException exception:
+
+                            await Task.CompletedTask;
+
+                            var myexception = exception;
+                            break;
+                    }
+
+                    break;
+
+            }
+        }
+    }
+}
